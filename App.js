a22f03789ba1708ddeaf2cd38a77fa9b5beea606
ashish sharma <ashishsharmastudio@gmail.com>
2023-07-09 19:58:35 -0700

    this is the way we use props

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  const [data,setData]=useState(0)
-  function updateData() 
-  {
-    setData(data+1)
-  }
-  console.warn("___________")
-  return (
+import Student from './student'
+function APP() {
+  return(
     <div className="App">
-      <h1>{data}</h1>
-      {/* <button> update Data</button> */}
-      <button onClick={updateData}>update Data</button>
+    <h1> props in react</h1>
+    <Student name={"ashu"} email="ashu@test.com" other={{address:'jhansi',mobile:"54151"}} />
+    <Student name={"raja"} email="RAJA@test.com" other={{address:'garutha',mobile:"54451"}} />
+    <Student name={"lali"} email="lali@test.com" other={{address:'shila',mobile:"545555151"}} />
     </div>
   );
 }
 
-export default App;
+export default APP;
